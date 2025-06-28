36bc84ad59b74362ca5c2e9f29dd785a1b265f5b
WindForest <munkored@yandex.ru>
2025-06-28 11:30:55 +0700

    added offsetdecoration

--- app/src/main/java/com/myschool/shiftcft/model/User.kt
+++ app/src/main/java/com/myschool/shiftcft/model/User.kt
@@ -3,5 +3,10 @@ package com.myschool.shiftcft.model
 data class User(
     val id: Long,
     val name: String,
+    val surname: String,
+    val birthday: String,
+    val phone: String,
+    val address: String,
+    val gender: String,
 
 )
